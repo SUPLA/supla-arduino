73708a8267d8225a8f6016a8cd41c69efeace600
Przemyslaw Zygmunt <p.zygmunt@acsoftware.pl>
2018-02-06 14:02:16 +0100

    license header

--- libraries/SuplaDevice/examples/SuplaEthernetShield_DallasTemperature/SuplaEthernetShield_DallasTemperature.ino
+++ libraries/SuplaDevice/examples/SuplaEthernetShield_DallasTemperature/SuplaEthernetShield_DallasTemperature.ino
@@ -1,3 +1,19 @@
+/*
+Copyright (C) AC SOFTWARE SP. Z O.O.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <OneWire.h>
@@ -5,9 +21,6 @@
 #include <SuplaDevice.h>
 
 /*
- * SUPLA DEVICE - ARDUINO - ETHERNET SHIELD & DS18B20 SENSOR
- * Author: Przemyslaw Zygmunt <przemek@supla.org>
- *
  * This example requires Dallas Temperature Control library installed. 
  * https://github.com/milesburton/Arduino-Temperature-Control-Library
  */
