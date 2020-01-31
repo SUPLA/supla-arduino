3cf84378b22c26a10e81aeadf3a0b1d42124cfca
Maya <34374601+ermaya@users.noreply.github.com>
2020-01-31 23:23:08 +0100

    Update PzemV3.h

--- libraries/SuplaDevice/supla/sensor/PzemV3.h
+++ libraries/SuplaDevice/supla/sensor/PzemV3.h
@@ -14,13 +14,13 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifndef _PzenV3_h
+#ifndef _PzemV3_h
 #define _PzemV3_h
 
 #include <Arduino.h>
 #include <PZEM004Tv30.h>
 #include <SoftwareSerial.h>
-#include "supla/sensor/one_phase_electricity_meter.h"
+#include "one_phase_electricity_meter.h"
 
 
 
@@ -38,20 +38,21 @@ class PZEMv3 : public OnePhaseElectricityMeter {
         }
 
        virtual void readValuesFromDevice() {
+	       
 	      	double current = pzem.current();
-        double voltage = pzem.voltage();
+    	 	double voltage = pzem.voltage();
 	      	double active = pzem.power();
 	      	double apparent = (voltage * current);
       		double reactive;
 	      	if (apparent > active) {reactive = sqrt(apparent * apparent - active * active);}
-    	     else {reactive =  0; }
+    	     	   else {reactive =  0; }
 		
-        setVoltage(0, voltage * 100);  
-        setCurrent(0, current * 1000);
-        setPowerActive(0,  active * 100000); 
-        setFwdActEnergy(0, pzem.energy() * 100000); 
-        setFreq( pzem.frequency() * 100);
-        setPowerFactor(0, pzem.pf() * 1000);
+        	setVoltage(0, voltage * 100);  
+        	setCurrent(0, current * 1000);
+        	setPowerActive(0,  active * 100000); 
+        	setFwdActEnergy(0, pzem.energy() * 100000); 
+        	setFreq( pzem.frequency() * 100);
+        	setPowerFactor(0, pzem.pf() * 1000);
       		setPowerApparent(0, apparent * 100000);
 	      	setPowerReactive(0, reactive* 10000);
         }
