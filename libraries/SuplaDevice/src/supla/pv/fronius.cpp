9a887652fbdfe7b35f34562f28d0408cf1529cbe
klew <krz.lewandowski@gmail.com>
2020-11-06 14:51:57 +0100

    Added fetching data from Fronius inverter with example

---
+++ libraries/SuplaDevice/src/supla/pv/fronius.cpp
@@ -0,0 +1,182 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#include "fronius.h"
+
+using namespace Supla;
+using namespace PV;
+
+enum ParametersToRead { NONE, TOTAL_ENERGY, FAC, IAC, PAC, UAC };
+
+Fronius::Fronius(IPAddress ip, int port)
+    : ip(ip),
+      port(port),
+      dataIsReady(false),
+      totalGeneratedEnergy(0),
+      currentPower(0),
+      currentCurrent(0),
+      currentFreq(0),
+      currentVoltage(0),
+      startCharFound(false),
+      bytesCounter(0),
+      dataFetchInProgress(false),
+      retryCounter(0),
+      valueToFetch(NONE) {
+}
+
+void Fronius::iterateAlways() {
+  if (dataFetchInProgress) {
+    if (!pvClient.connected()) {
+      Serial.println(F("Fronius fetch completed"));
+      dataFetchInProgress = false;
+      dataIsReady = true;
+    }
+    if (pvClient.available()) {
+      Serial.print(F("Reading data from Fronius: "));
+      Serial.println(pvClient.available());
+    }
+    while (pvClient.available()) {
+      char c;
+      c = pvClient.read();
+      if (c == '\n') {
+        if (startCharFound) {
+          if (bytesCounter > 79) bytesCounter = 79;
+          buf[bytesCounter] = '\0';
+          char varName[80];
+          char varValue[80];
+          sscanf(buf, " %s  : %s", varName, varValue);
+          if (valueToFetch != NONE && strncmp(varName, "Value", strlen("Value")) == 0) {
+            switch (valueToFetch) {
+              case TOTAL_ENERGY: {
+                float totalProd = atof(varValue);
+                Serial.print(F("Total production: "));
+                Serial.print(totalProd);
+                Serial.println(F(" Wh"));
+                totalGeneratedEnergy = totalProd * 100;
+
+                break;
+              }
+              case PAC: {
+                float curPower = atof(varValue);
+                Serial.print(F("Current power: "));
+                Serial.println(curPower);
+                currentPower = curPower * 100000;
+
+                break;
+              }
+              case IAC: {
+                float curCurrent = atof(varValue);
+                Serial.print(F("Current: "));
+                Serial.println(curCurrent);
+                currentCurrent = curCurrent * 1000;
+
+                break;
+              }
+              case FAC: {
+                float curFreq = atof(varValue);
+                Serial.print(F("Frequency: "));
+                Serial.println(curFreq);
+                currentFreq = curFreq * 100;
+
+                break;
+              }
+              case UAC: {
+                float curVoltage = atof(varValue);
+                Serial.print(F("Voltage: "));
+                Serial.println(curVoltage);
+                currentVoltage = curVoltage * 100;
+
+                break;
+              }
+            }
+            valueToFetch = NONE;
+          } else if (strncmp(varName, "TOTAL_ENERGY\"", strlen("TOTAL_ENERGY")) == 0) {
+            valueToFetch = TOTAL_ENERGY;
+          } else if (strncmp(varName, "FAC", strlen("FAC")) == 0) {
+            valueToFetch = FAC;
+          } else if (strncmp(varName, "UAC", strlen("UAC")) == 0) {
+            valueToFetch = UAC;
+          } else if (strncmp(varName, "IAC", strlen("IAC")) == 0) {
+            valueToFetch = IAC;
+          } else if (strncmp(varName, "PAC", strlen("PAC")) == 0) {
+            valueToFetch = PAC;
+          }
+        }
+        bytesCounter = 0;
+        startCharFound = false;
+      } else if (c == '"' || startCharFound) {
+        startCharFound = true;
+        if (c == '"') {
+          c = ' ';
+        }
+        if (bytesCounter < 80) {
+          buf[bytesCounter] = c;
+        }
+        bytesCounter++;
+      }
+    }
+    if (!pvClient.connected()) {
+      pvClient.stop();
+    }
+  }
+  if (dataIsReady) {
+    dataIsReady = false;
+    setFwdActEnergy(0, totalGeneratedEnergy);
+    setPowerActive(0, currentPower);
+    setCurrent(0, currentCurrent);
+    setVoltage(0, currentVoltage);
+    setFreq(currentFreq);
+    updateChannelValues();
+  }
+}
+
+bool Fronius::iterateConnected(void *srpc) {
+  if (!dataFetchInProgress) {
+    if (lastReadTime == 0 || millis() - lastReadTime > 15000) {
+      lastReadTime = millis();
+      Serial.println(F("Fronius connecting"));
+      if (pvClient.connect(ip, port)) {
+        retryCounter = 0;
+        dataFetchInProgress = true;
+        Serial.println(F("Succesful connect"));
+
+        pvClient.println("GET /solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceID=1&DataCollection=CommonInverterData HTTP/1.1");
+        pvClient.println("Host: localhost");
+        pvClient.println("Connection: close");
+        pvClient.println();
+
+      } else {  // if connection wasn't successful, try few times. If it fails,
+                // then assume that inverter is off during the night
+        Serial.print(F("Failed to connect to Fronius at: "));
+        Serial.print(ip);
+        Serial.print(F(":"));
+        Serial.println(port);
+        retryCounter++;
+        if (retryCounter > 3) {
+          currentPower = 0;
+          currentFreq = 0;
+          currentCurrent = 0;
+          currentVoltage = 0;
+          dataIsReady = true;
+        }
+      }
+    }
+  }
+  return Element::iterateConnected(srpc);
+}
+
+void Fronius::readValuesFromDevice() {
+}
