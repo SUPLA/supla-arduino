896d84e3fbbca6ce18aed207a84650d25d0f5b83
Krzysztof Lewandowski <krz.lewandowski@gmail.com>
2022-01-03 14:09:07 +0100

    Added supla/clock to compilation on all targets

--- extras/esp-idf/supla-device/component.mk
+++ extras/esp-idf/supla-device/component.mk
@@ -1,7 +1,10 @@
 COMPONENT_ADD_INCLUDEDIRS := ../../../src
 #COMPONENT_OBJS := file1.o file2.o thing/filea.o thing/fileb.o anotherthing/main.o
 
-COMPONENT_SRCDIRS := ../../../src/supla-common ../../../src/supla ../../../src/supla/conditions
+COMPONENT_SRCDIRS := ../../../src/supla-common \
+										 ../../../src/supla \
+										 ../../../src/supla/conditions \
+										 ../../../src/supla/clock
 
 CFLAGS += -DSUPLA_DEVICE
 CPPFLAGS += -DSUPLA_DEVICE
